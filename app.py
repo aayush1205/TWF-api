cb8c8fa26034cff6273e47c15a7b2b6939f0d59f
aayush1205 <upadhyayaayush12@gmail.com>
2021-09-23 19:14:06 +0530

    GET, POST

--- app.py
+++ app.py
@@ -4,18 +4,22 @@ from flask import Flask, request, abort
 app = Flask(__name__)
 
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['GET', 'POST'])
 def solve():
-    req = request.get_json()
-    try:
-        inp = req["input"]
-    except:
-        abort(400)
-    with open("input.txt", "w+") as f:
-        f.write(inp)
-    stream = os.popen('./a.out < input.txt')
-    output = stream.read()
-    return output, 200
+    if request.method == 'GET':
+        return "<h1>Welcome!</h1>"
+    else:
+
+        req = request.get_json()
+        try:
+            inp = req["input"]
+        except:
+            abort(400)
+        with open("input.txt", "w+") as f:
+            f.write(inp)
+        stream = os.popen('./a.out < input.txt')
+        output = stream.read()
+        return output, 200
 
 
 if __name__ == "__main__":
